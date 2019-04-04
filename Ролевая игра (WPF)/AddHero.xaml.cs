31517dfd397478362088e434408a16a716518827
Кирилл Максимук <maksimuk.irill@gmail.com>
2019-04-04 10:46:54 +0300

    No Description

---
+++ Ролевая игра (WPF)/AddHero.xaml.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ролевая_игра__WPF_
+{
+    public partial class AddHero : Window
+    {
+        uint RecievedID;
+
+        bool IsAgeAcceptable = false;
+        bool IsNameAcceptable = false;
+        bool IsMagicChecked = false;
+
+        Персонаж Новый_персонаж = null;
+        Персонаж_с_магией Новый_персонаж_с_магией = null;
+
+        public string Имя { get; private set; }
+        public string Раса { get; private set; }
+        public uint Возраст { get; private set; }
+        public bool Пол { get; private set; }
+
+        public AddHero(int ID)
+        {
+            InitializeComponent();
+            RecievedID = (uint)ID;
+            GroupBox_Race.Visibility = Visibility.Hidden;
+            GroupBox_HaveMagic.Visibility = Visibility.Hidden;
+        }
+
+        public Персонаж Получить_Персонажа() => Новый_персонаж;
+        public Персонаж_с_магией Получить_Персонажа_с_магией() => Новый_персонаж_с_магией;
+
+        private void Button_Accept_Click(object sender, RoutedEventArgs e)
+        {
+            if (Hero_DoHaveMagic.IsChecked == true)
+            {
+                Новый_персонаж_с_магией = new Персонаж_с_магией(RecievedID, Имя, Пол, Возраст, Раса);
+            }
+            else
+            {
+                Новый_персонаж = new Персонаж(RecievedID, Имя, Пол, Возраст, Раса);
+            }
+            DialogResult = true;
+            Close();
+        }
+
+        void TextBlock_Clear(object sender, MouseEventArgs e)
+        {
+            TextBlock_Description.Text = "";
+        }
+        void TextBlock_Change(string Text)
+        {
+            TextBlock_Description.Text = Text;
+        }
+
+        private void RadioButton_Hero_Male_Click(object sender, RoutedEventArgs e)
+        {
+            GroupBox_Race.Visibility = Visibility.Visible;
+            RadioButton_Race_Elf.Content = "Эльф";
+            Пол = true;
+            CheckAllRequaredFields();
+        }
+        private void RadioButton_Hero_Female_Click(object sender, RoutedEventArgs e)
+        {
+            GroupBox_Race.Visibility = Visibility.Visible;
+            RadioButton_Race_Elf.Content = "Эльфийка";
+            Пол = false;
+            CheckAllRequaredFields();
+        }
+
+        //проверка ввода возраста
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e) 
+        {
+            bool AgeHasInvalidInput = false;
+
+            if (TextBox_Age.Text.Length == 0)
+                AgeHasInvalidInput = true;
+
+            foreach (char symbol in TextBox_Age.Text)
+            {
+                if (!char.IsDigit(symbol))
+                {
+                    AgeHasInvalidInput = true;
+                    break;
+                }
+            }
+
+            if (AgeHasInvalidInput == false)
+            {
+                IsAgeAcceptable = true;
+                Возраст = Convert.ToUInt32(TextBox_Age.Text);
+            }
+            else
+                IsAgeAcceptable = false;
+
+            CheckAllRequaredFields();
+        }
+        //проверка ввода имени
+        private void TextBox_NameOfHero_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool NameHasInvalidInput = false;
+            char PreviousSymbol = 'a';
+            foreach (char symbol in TextBox_NameOfHero.Text)
+            {
+                if (!char.IsLetter(symbol) || symbol == ' ' && PreviousSymbol == ' ')
+                {
+                    NameHasInvalidInput = true;
+                    break;
+                }
+                PreviousSymbol = symbol;
+            }
+            if (NameHasInvalidInput == false)
+            {
+                IsNameAcceptable = true;
+                Имя = TextBox_NameOfHero.Text;
+            }
+            else
+                IsNameAcceptable = false;
+            CheckAllRequaredFields();
+        }
+
+        void CheckAllRequaredFields()
+        {
+            if (IsNameAcceptable && IsAgeAcceptable && IsMagicChecked)
+                Button_Accept.IsEnabled = true;
+            else
+                Button_Accept.IsEnabled = false;
+        }
+
+        private void RadioButton_Race_Human_Checked(object sender, RoutedEventArgs e)
+        {
+            GroupBox_HaveMagic.Visibility = Visibility.Visible;
+            Раса = "человек";
+            CheckAllRequaredFields();
+        }
+        private void RadioButton_Race_Dwarf_Checked(object sender, RoutedEventArgs e)
+        {
+            GroupBox_HaveMagic.Visibility = Visibility.Visible;
+            Раса = "гном";
+            CheckAllRequaredFields();
+        }
+        private void RadioButton_Race_Elf_Checked(object sender, RoutedEventArgs e)
+        {
+            GroupBox_HaveMagic.Visibility = Visibility.Visible;
+            if (Пол == true)
+                Раса = "эльф";
+            else
+                Раса = "эльфийка";
+            CheckAllRequaredFields();
+        }
+        private void RadioButton_Race_Ork_Checked(object sender, RoutedEventArgs e)
+        {
+            GroupBox_HaveMagic.Visibility = Visibility.Visible;
+            Раса = "орк";
+            CheckAllRequaredFields();
+        }
+        private void RadioButton_Race_Goblin_Checked(object sender, RoutedEventArgs e)
+        {
+            GroupBox_HaveMagic.Visibility = Visibility.Visible;
+            Раса = "гоблин";
+            CheckAllRequaredFields();
+        }
+        private void Hero_DoHaveMagic_Click(object sender, RoutedEventArgs e)
+        {
+            IsMagicChecked = true;
+            CheckAllRequaredFields();
+        }
+        private void Hero_DoNotHaveMagic_Click(object sender, RoutedEventArgs e)
+        {
+            IsMagicChecked = true;
+            CheckAllRequaredFields();
+        }
+
+        private void TextBox_MouseEnter(object sender, MouseEventArgs e)
+        {
+            TextBlock_Change("К истории о противостоянии присоединяется новый герой! \nО ком будут слагать эпические легенды?");
+        }
+        private void RadioButton_Hero_Male_MouseEnter(object sender, MouseEventArgs e)
+        {
+            TextBlock_Change($"Благородный сэр с гордым именем {TextBox_NameOfHero.Text} и в сияющих доспехах");
+        }
+        private void RadioButton_Hero_Female_MouseEnter(object sender, MouseEventArgs e)
+        {
+            TextBlock_Change($"Благородная мэм с гордым именем {TextBox_NameOfHero.Text} и в сияющих доспехах");
+        }
+        private void Button_Accept_MouseEnter(object sender, MouseEventArgs e)
+        {
+            TextBlock_Change($"Готовы отправится на поиски приключений?");
+        }
+        private void RadioButton_Race_Human_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Пол)
+            {
+                TextBlock_Change($"Храбрый герой, с благородным именем {Имя}, оказался человеком! \n" +
+                $"Самым средним и не выдающимся представителем мифического мира");
+            }
+            else
+            {
+                TextBlock_Change($"Храбрая героиня, с благородным именем {Имя}, оказалась человеком! \n" +
+                $"Самым средним и не выдающимся представителем мифического мира");
+
+            }
+        }
+        private void RadioButton_Race_Dwarf_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Пол)
+            {
+                TextBlock_Change($"Не позволяйте себе быть обманутым его ростом, он много кому сможет задать жару");
+
+            }
+            else
+            {
+                TextBlock_Change($"Не позволяйте себе быть обманутым её ростом, она много кому сможет задать жару");
+
+            }
+        }
+        private void RadioButton_Race_Elf_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Пол)
+            {
+                TextBlock_Change($"Высокие (и чаще высокомерные) мастера театра боевых действий с острыми ушками и не менее острым языком");
+
+            }
+            else
+            {
+                TextBlock_Change($"Высокие (и чаще высокомерные) мастерицы театра боевых действий с острыми ушками и не менее острым языком");
+
+            }
+        }
+        private void RadioButton_Race_Ork_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Пол)
+            {
+                TextBlock_Change($"Орки, могучие и свирепые берсерки с жаждой крови, но обделенные интеллектом");
+
+            }
+            else
+            {
+                TextBlock_Change($"Орки, могучие и свирепые берсерки с жаждой крови, но обделенные интеллектом");
+
+            }
+        }
+        private void RadioButton_Race_Goblin_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Пол)
+            {
+                TextBlock_Change($"Гоблины славятся своими умениями как инженеры (хоть их изобретения так и норовят развалиться " +
+                    $"или взорваться), так и как торговцы, за счет своей хитрости");
+            }
+            else
+            {
+                TextBlock_Change($"Гоблины славятся своими умениями как инженеры (хоть их изобретения так и норовят развалиться " +
+                    $"или взорваться), так и как торговцы, за счет своей хитрости");
+            }
+        }
+        private void StackPanel_MouseEnter(object sender, MouseEventArgs e)
+        {
+            TextBlock_Change($"Будет ли {Имя} обладать магией?");
+        }
+    }
+}
---
+++ Ролевая игра (WPF)/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace Ролевая_игра__WPF_
+{
+    public partial class MainWindow : Window
+    {
+        private AddHero AddHeroWindow;
+        private List<object> Персонажи = new List<object>();
+        private string ConsoleBuffer;
+        private int ТекущийПерсонаж;
+        private bool? IsRecentlyAddedHeroMage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Console.Text = "Добро пожаловать, герой. Я твой рассказчик, Система Одностороннего Повествования \nИстории, но можешь называть меня СОПИ ♥. \nДля начала предалагаю начать с создания твоего героя.\n";
+            ТекущийПерсонаж = 0;
+        }
+
+        private void MainWindow_Button_AddHero_Click(object sender, RoutedEventArgs e)
+        {
+            AddHeroWindow = new AddHero(Персонажи.Count - 1);
+            AddHeroWindow.ShowDialog();
+            bool? dialogResult = AddHeroWindow.DialogResult;
+            bool flag = true;
+            if (dialogResult.GetValueOrDefault() == flag & dialogResult.HasValue)
+            {
+                Персонаж ВозможныйПерсонаж = AddHeroWindow.Получить_Персонажа();
+                if (ВозможныйПерсонаж != null)
+                {
+                    Персонажи.Add(AddHeroWindow.Получить_Персонажа());
+                    bool пол = AddHeroWindow.Пол;
+                    if (пол)
+                    {
+                        Console.Text = $"Появился новый герой! Он {((Персонажи[Персонажи.Count - 1] as Персонаж) as Персонаж).Раса} в возрасте " +
+                            $"{Персонаж.Лет_Лета_Год((Персонажи[Персонажи.Count - 1] as Персонаж).Возраст)} и зовут его {(Персонажи[Персонажи.Count - 1] as Персонаж).Имя}\n";
+                    }
+                    else
+                    {
+                        Console.Text = $"Появилась новая героиня! Она {(Персонажи[Персонажи.Count - 1] as Персонаж).Раса} в возрасте " +
+                            $"{Персонаж.Лет_Лета_Год((Персонажи[Персонажи.Count - 1] as Персонаж).Возраст)} и зовут её {(Персонажи[Персонажи.Count - 1] as Персонаж).Имя}\n";
+                    }
+                    IsRecentlyAddedHeroMage = new bool?(false);
+                }
+                else //Персонажи с магией
+                {
+                    Персонажи.Add(AddHeroWindow.Получить_Персонажа_с_магией());
+                    bool пол2 = AddHeroWindow.Пол;
+                    if (пол2)
+                    {
+                        Console.Text = $"Появился новый герой! Он {(Персонажи[Персонажи.Count - 1] as Персонаж_с_магией).Раса} с познаниями в магии и в возрасте " +
+                            $"{Персонаж.Лет_Лета_Год((Персонажи[Персонажи.Count - 1] as Персонаж_с_магией).Возраст)}, а зовут его {(Персонажи[Персонажи.Count - 1] as Персонаж_с_магией).Имя}\n";
+                    }
+                    else
+                    {
+                        Console.Text = $"Появилась новая героиня! Она {(Персонажи[Персонажи.Count - 1] as Персонаж_с_магией).Раса} с познаниями в магии и в возрасте " +
+                            $"{Персонаж.Лет_Лета_Год((Персонажи[Персонажи.Count - 1] as Персонаж_с_магией).Возраст)}, а зовут её {(Персонажи[Персонажи.Count - 1] as Персонаж_с_магией).Имя}\n";
+                    }
+                    IsRecentlyAddedHeroMage = new bool?(true);
+                }
+                MainWindow_Button_ShowInfo.IsEnabled = true;
+                ТекущийПерсонаж = Персонажи.Count-1;
+            }
+            if (Персонажи.Count >= 2)
+            {
+                MainWindow_Button_SwitchHero.IsEnabled = true;
+            }
+        }
+
+        private void MainWindow_Button_ShowInfo_Click(object sender, RoutedEventArgs e)
+        {
+            if ((string)MainWindow_Button_ShowInfo.Content == "Вернуться к \nповествованию")
+            {
+                MainWindow_Button_ShowInfo.Content = "Состояние текущего \nгероя";
+                Console.Text = ConsoleBuffer;
+            }
+            else
+            {
+                MainWindow_Button_ShowInfo.Content = "Вернуться к \nповествованию";
+                ConsoleBuffer = Console.Text;
+                if (IsRecentlyAddedHeroMage == false)
+                {
+                    Console.Text = (Персонажи[ТекущийПерсонаж] as Персонаж).ToString();
+                }
+                else //Персонажи с магией
+                {
+                    Console.Text = (Персонажи[ТекущийПерсонаж] as Персонаж_с_магией).ToString();
+                }
+            }
+        }
+
+        private void MainWindow_Button_SwitchHero_Click(object sender, RoutedEventArgs e)
+        {
+            if (ТекущийПерсонаж == Персонажи.Count-1)
+            {
+                ТекущийПерсонаж = 0;
+                Console.Text += $"Инициативу принимает {(Персонажи[ТекущийПерсонаж] as Персонаж).Имя}\n";
+            }
+            else
+            {
+                ТекущийПерсонаж++;
+                Console.Text += $"Инициативу принимает {(Персонажи[ТекущийПерсонаж] as Персонаж).Имя}\n";
+            }
+        }
+    }
+}
